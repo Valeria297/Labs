ce5b30bf28d4d1609fbc88e8f88205df1a58022e
Valeria297 <valeriserbin2907@gmail.com>
2022-10-14 14:35:08 +0300

    labs

--- Labs/Lab6.cpp
+++ Labs/Lab6.cpp
@@ -1,34 +1,45 @@
-//ввести строку символов. определить длину введенной строки L, и если длина кратна 3, то удал€ютс€ все цифры, дел€щиес€ на 3
-
-//дана строка, состо€ща€ из слов на английском €зыке, разделенных пробелами. вывести на экран слова в пор€дке алфавита.
-
 #include <iostream>
 #include <math.h>
+#include <algorithm> 
 using namespace std;
 
 /*  You can see the sixth lab work here!
 	11 variant is used in both levels.
 	   */
 
-int main()
-{
-	//First level
-	cout << "This is the first level task!" << endl;
-
-	cout << "Input a string: " << endl;
-	char a[102];
-	cin.getline(a, 102, '\n');
-
-	int len = strlen(a);
-
-	if (len % 3 == 0) {
-		for (int i = 0; i < len; i++) {
-			if (a[i] == '3' || a[i] == '6' || a[i] == '9') 
-				continue;
-			cout << a[i];
-		}
-	}
-	else cout << "The length is "<< len << "\nNon-compliance with the conditions!" << endl;
-	
-	
-}
+//int main()
+//{
+//	//First level
+//	cout << "This is the first level task!" << endl;
+//
+//	cout << "Input a string: " << endl;
+//	char a[102];
+//	cin.getline(a, 102);
+//
+//	int len = strlen(a);
+//
+//	if (len % 3 == 0) {
+//		cout << " -> ";
+//		for (int i = 0; i < len; i++) {
+//			if (a[i] == '3' || a[i] == '6' || a[i] == '9') 
+//				continue;
+//			cout << a[i];
+//		}
+//		cout << "\n";
+//	}
+//	else cout << "The length is "<< len << "\nNon-compliance with the conditions!" << endl;
+//	
+//	//Second level
+//	cout << "\nThis is the second level task!" << endl;
+//
+//	string arr[100], word;
+//
+//	int size = 0;
+//	while (cin >> word) {
+//		if (word == "0") break;
+//		arr[size++] = word;
+//	}
+//	sort(arr, arr + size); 
+//
+//	for (int i = 0; i < size; i++) cout << arr[i] << endl;
+//}
---
+++ Labs/Lab7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*  You can see the seventh lab work here!
+	11 variant is used in both levels.
+	   */
+
+int main()
+{
+	//First level
+	cout << "This is the first level task!" << endl;
+
+
+}
